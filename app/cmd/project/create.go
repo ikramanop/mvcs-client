a3c170fb9c493735c3da72f4e42a5ab84a0d2b3a
PavelArs <46156007+PavelArs@users.noreply.github.com>
2023-05-21 19:40:42 +0300

    WIP creation subcommand

--- app/cmd/project/create.go
+++ app/cmd/project/create.go
@@ -6,15 +6,25 @@ import (
 	"errors"
 	"fmt"
 	"github.com/google/uuid"
-	"github.com/ikramanop/mvcs-client/app/utility"
-
-	"github.com/spf13/cobra"
-
 	"github.com/ikramanop/mvcs-client/app"
 	"github.com/ikramanop/mvcs-client/app/model"
+	"github.com/ikramanop/mvcs-client/app/utility"
+	"github.com/spf13/cobra"
 )
 
-const ProjectCreateProcessError = "ошибка в процессе создания проекта"
+const (
+	ProjectAlreadyExistsOnServer = "проект уже есть на remote"
+	ProjectAlreadyExistsLocally  = "проект уже есть на local"
+	ProjectCreateProcessError    = "ошибка в процессе создания проекта"
+	BranchAlreadyExistsOnServer  = "ветка уже есть на remote"
+	BranchAlreadyExistsLocally   = "ветка уже есть на local"
+	BranchCreateProcessError     = "ошибка в процессе создания ветки"
+)
+
+type Project struct {
+	Identifier string `protobuf:"bytes,1,opt,name=identifier,proto3" json:"identifier,omitempty"`
+	Name       string `protobuf:"bytes,2,opt,name=name,proto3" json:"name,omitempty"`
+}
 
 var ProjectCreateCMD = &cobra.Command{
 	Use:   "create",
@@ -54,36 +64,64 @@ func create() error {
 		}
 	}
 
-	// тут надо проверить, что на сервере есть проект
-	// если есть, то нужно закрывать процесс, тк мы не можем создавать два проекта с одним именем
-	// если нет, то продолжаем процесс
+	ctx, cancel := c.GetContext(false)
+	defer cancel()
 
+	// чекаем на сервере наличие проекта с таким же именем
+	serverProject, err := c.Projects.Find(ctx, wd)
+	if err != nil {
+		return model.WrapError(ProjectCreateProcessError, err)
+	}
+	if serverProject != nil {
+		return model.WrapError(ProjectAlreadyExistsOnServer, err)
+	}
+
+	// создаем локально
 	if err = c.DB.CreateProject(&model.ProjectParams{Name: wd}); err != nil {
 		return model.WrapError(ProjectCreateProcessError, err)
 	}
 
-	project, err := c.DB.GetProjectByName(&model.ProjectParams{Name: wd})
+	dbProject, err := c.DB.GetProjectByName(&model.ProjectParams{Name: wd})
 	if err != nil {
 		return model.WrapError(ProjectCreateProcessError, err)
 	}
 
-	fmt.Printf("Инициализирован проект %s в директории %s\n", project.Name, wd)
+	fmt.Printf("Инициализирован проект %s в директории %s на local\n", dbProject.Name, wd)
 
+	// создаем на сервере
+	serverProject, err = c.Projects.Create(ctx, dbProject.Name)
+	if err != nil {
+		return model.WrapError(ProjectCreateProcessError, err)
+	}
+
+	fmt.Printf("Инициализирован проект %s в директории %s на remote\n", dbProject.Name, wd)
+
+	// создаем ветку локально
 	if err = c.DB.CreateBranch(&model.BranchParams{
 		Name:           "master",
-		ProjectId:      project.Id,
+		ProjectId:      dbProject.Id,
 		UserIdentifier: c.Cfg.Auth.Login,
 		ParentBranchId: sql.NullInt32{},
 	}); err != nil {
-		return model.WrapError(ProjectCreateProcessError, err)
+		return model.WrapError(BranchCreateProcessError, err)
 	}
 
-	branch, err := c.DB.GetBranchByName(&model.BranchParams{Name: "master"})
+	dbBranch, err := c.DB.GetBranchByName(&model.BranchParams{Name: "master"})
 	if err != nil {
-		return model.WrapError(ProjectCreateProcessError, err)
+		return model.WrapError(BranchCreateProcessError, err)
 	}
 
-	fmt.Printf("Инициализирована ветка %s\n", branch.Name)
+	fmt.Printf("Инициализирована ветка %s на local\n", dbBranch.Name)
+
+	// создаем на сервере
+	_, err = c.Project.CreateBranch(ctx, dbBranch.Name, nil)
+	if err != nil {
+		return model.WrapError(BranchCreateProcessError, err)
+	}
+
+	fmt.Printf("Инициализирована ветка %s на remote\n", dbProject.Name)
+
+	//todo дальше отправить запросы на создание версий веток на сервере
 
 	fileStructure, err := c.Diff.GetFileStructureWithHashesAndInfo()
 	if err != nil {
@@ -92,7 +130,7 @@ func create() error {
 
 	for file, scam := range fileStructure {
 		if err := c.DB.CreateFile(&model.FileParams{
-			BranchId: branch.Id,
+			BranchId: dbBranch.Id,
 			FilePath: file,
 		}); err != nil {
 			return model.WrapError(ProjectCreateProcessError, err)
@@ -125,7 +163,7 @@ func create() error {
 	}
 
 	if err := c.DB.CreateBranchVersion(&model.BranchVersionParams{
-		BranchId:       branch.Id,
+		BranchId:       dbBranch.Id,
 		Uuid:           uuid.New().String(),
 		FilesStructure: string(extractedTreeJson),
 	}); err != nil {
@@ -133,12 +171,6 @@ func create() error {
 	}
 
 	fmt.Println("Версии файлов зафиксированы для ветки master")
-
-	// тут нужно добавить отправку файлов на сервер
-	// методы тут https://github.com/Amir0715/MVCS-Server/blob/main/MVCS/MVCS.Presentation.gRPC/Protos/projects.proto
-	// нужно для каждого написать оболочку в client, прокинуть в container и вызывать здесь обёрнутые методы
-
-	// НУЖНО ДОБАВИТЬ ОТПРАВКУ ДАННЫХ НА СЕРВЕР, ЛОКАЛЬНО ВСЁ ГОТОВО
 
 	return nil
 }
