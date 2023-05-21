8b0c6d1efdafe2a2f65d51ec4ec2548f5fea14b2
PavelArs <46156007+PavelArs@users.noreply.github.com>
2023-05-21 21:22:03 +0300

    some more code refactoring

--- app/cmd/project/create.go
+++ app/cmd/project/create.go
@@ -9,6 +9,7 @@ import (
 	"github.com/google/uuid"
 	"github.com/ikramanop/mvcs-client/app"
 	"github.com/ikramanop/mvcs-client/app/model"
+	"github.com/ikramanop/mvcs-client/app/protobuf/messages"
 	"github.com/ikramanop/mvcs-client/app/utility"
 	"github.com/spf13/cobra"
 )
@@ -57,11 +58,13 @@ func create() error {
 	ctx, cancel := c.GetContext(false)
 	defer cancel()
 
+	// получаем local и remote проекты
 	project, err := CreateProject(c, ctx, wd)
 	if err != nil {
 		return model.WrapError(ProjectCreateProcessError, err)
 	}
 
+	// создаем master ветки в local и remote проектах
 	branch, err := CreateBranch(c, project, ctx)
 	if err != nil {
 		return model.WrapError(ProjectCreateProcessError, err)
@@ -69,6 +72,17 @@ func create() error {
 
 	//todo дальше отправить запросы на создание версий веток на сервере
 
+	err = AddFilesToLocalDB(c, branch)
+	if err != nil {
+		return model.WrapError(ProjectCreateProcessError, err)
+	}
+
+	fmt.Println("Версии файлов зафиксированы для ветки master")
+
+	return nil
+}
+
+func AddFilesToLocalDB(c *app.Container, branch *model.Branch) error {
 	fileStructure, err := c.Diff.GetFileStructureWithHashesAndInfo()
 	if err != nil {
 		return model.WrapError(ProjectCreateProcessError, err)
@@ -116,13 +130,34 @@ func create() error {
 		return model.WrapError(ProjectCreateProcessError, err)
 	}
 
-	fmt.Println("Версии файлов зафиксированы для ветки master")
-
 	return nil
 }
 
 func CreateBranch(c *app.Container, project *model.Project, ctx context.Context) (*model.Branch, error) {
 	// создаем ветку локально
+	localBranch, err := CreateLocalBranch(c, project)
+	if err != nil {
+		return nil, model.WrapError(BranchCreateProcessError, err)
+	}
+
+	fmt.Printf("Инициализирована ветка %s на local\n", localBranch.Name)
+
+	// создаем на сервере
+	remoteBranch, err := c.Project.CreateBranch(ctx, localBranch.Name, nil)
+	if err != nil {
+		return nil, model.WrapError(BranchCreateProcessError, err)
+	}
+	// тут наверное можно по айди, а не по имени
+	if remoteBranch.Name != localBranch.Name {
+		return nil, model.WrapError(BranchCreateProcessError, err)
+	}
+
+	fmt.Printf("Инициализирована ветка %s на remote\n", localBranch.Name)
+
+	return localBranch, nil
+}
+
+func CreateLocalBranch(c *app.Container, project *model.Project) (*model.Branch, error) {
 	if err := c.DB.CreateBranch(&model.BranchParams{
 		Name:           "master",
 		ProjectId:      project.Id,
@@ -132,55 +167,79 @@ func CreateBranch(c *app.Container, project *model.Project, ctx context.Context)
 		return nil, model.WrapError(BranchCreateProcessError, err)
 	}
 
-	localBranch, err := c.DB.GetBranchByName(&model.BranchParams{Name: "master"})
+	branch, err := c.DB.GetBranchByName(&model.BranchParams{Name: "master"})
 	if err != nil {
 		return nil, model.WrapError(BranchCreateProcessError, err)
 	}
 
-	fmt.Printf("Инициализирована ветка %s на local\n", localBranch.Name)
-
-	// создаем на сервере
-	_, err = c.Project.CreateBranch(ctx, localBranch.Name, nil)
-	if err != nil {
-		return nil, model.WrapError(BranchCreateProcessError, err)
-	}
-
-	fmt.Printf("Инициализирована ветка %s на remote\n", project.Name)
-
-	return localBranch, nil
+	return branch, err
 }
 
 func CreateProject(c *app.Container, ctx context.Context, wd string) (*model.Project, error) {
 	// чекаем на сервере наличие проекта с таким же именем
+	// и создаем на сервере, если там такого проекта нет
+	remoteProject, err := CreateRemoteProject(c, ctx, wd)
+	if err != nil {
+		return nil, model.WrapError(ProjectCreateProcessError, err)
+	}
+
+	// создаем локально
+	localProject, err := CreateLocalProject(c, wd)
+	if err != nil {
+		return nil, model.WrapError(ProjectCreateProcessError, err)
+	}
+
+	if remoteProject.Name != localProject.Name {
+		return nil, model.WrapError(ProjectCreateProcessError, err)
+	}
+
+	return localProject, nil
+}
+
+func CreateLocalProject(c *app.Container, wd string) (*model.Project, error) {
+	if err := c.DB.CreateProject(&model.ProjectParams{Name: wd}); err != nil {
+		return nil, model.WrapError(ProjectCreateProcessError, err)
+	}
+
+	project, err := c.DB.GetProjectByName(&model.ProjectParams{Name: wd})
+	if err != nil {
+		return nil, model.WrapError(ProjectCreateProcessError, err)
+	}
+
+	fmt.Printf("Инициализирован проект %s в директории %s на local\n", project.Name, wd)
+
+	return project, nil
+}
+
+func CreateRemoteProject(c *app.Container, ctx context.Context, wd string) (*messages.Project, error) {
+	project, err := CheckRemoteProjectExists(c, ctx, wd)
+	if err != nil {
+		return nil, model.WrapError(ProjectCreateProcessError, err)
+	}
+	if project != nil {
+		fmt.Printf("Проект %s уже инициализирован в директории %s на remote\n", project.Name, wd)
+		return project, nil
+	}
+
+	project, err = c.Projects.Create(ctx, wd)
+	if err != nil {
+		return nil, model.WrapError(ProjectCreateProcessError, err)
+	}
+
+	fmt.Printf("Инициализирован проект %s в директории %s на local\n", project.Name, wd)
+
+	return project, nil
+}
+
+func CheckRemoteProjectExists(c *app.Container, ctx context.Context, wd string) (*messages.Project, error) {
 	serverProject, err := c.Projects.Find(ctx, wd)
 	if err != nil {
 		return nil, model.WrapError(ProjectCreateProcessError, err)
 	}
 	if serverProject != nil {
-		return nil, model.WrapError(ProjectAlreadyExistsOnServer, err)
+		return nil, nil
 	}
-
-	// создаем локально
-	if err = c.DB.CreateProject(&model.ProjectParams{Name: wd}); err != nil {
-		return nil, model.WrapError(ProjectCreateProcessError, err)
-	}
-
-	localProject, err := c.DB.GetProjectByName(&model.ProjectParams{Name: wd})
-	if err != nil {
-		return nil, model.WrapError(ProjectCreateProcessError, err)
-	}
-
-	fmt.Printf("Инициализирован проект %s в директории %s на local\n", localProject.Name, wd)
-
-	// создаем на сервере
-	serverProject, err = c.Projects.Create(ctx, localProject.Name)
-	if err != nil {
-		return nil, model.WrapError(ProjectCreateProcessError, err)
-	}
-
-	fmt.Printf("Инициализирован проект %s в директории %s на remote\n", localProject.Name, wd)
-
-	return localProject, nil
+	return serverProject, nil
 }
 
 func GetWorkingDirectory(c *app.Container) (string, error) {
